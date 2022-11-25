bffa40f5dc672288f3ad4cda58f2d078616ce89c
johnfrancesalcano <johnfrancesalcano@gmail.com>
2022-11-25 18:16:29 +0800

    SECTION 12, VIDEO 283, PRACTICE DAY

--- exercise.js
+++ exercise.js
@@ -10,24 +10,43 @@ const secondButtonElement = document.getElementById('adding-color');
 //    The functions should "console.dir()" the clicked buttons.
 //    - Output the first button by using the variable in which it's stored
 //    - Output the second button WITHOUT using the variable in which it's stored
+// function removingParagraph() {
+//     console.dir(firstButtonElement);
+// }
 
-function 
+// function addingBackgroundColor(event) {
+//     console.dir(event.target);
+// }
 
-firstButtonElement.addEventListener('click', )
 
-secondButtonElement.addEventListener('click', )
+// firstButtonElement.addEventListener('click',removingParagraph );
+// secondButtonElement.addEventListener('click', addingBackgroundColor);
 
 // 3) Now select and store the paragraphs mentioned in the text you see on the page
 //    (first and third paragraph)
 //    - Select BOTH paragraphs by drilling into the document and "navigating" to the
 //      mentioned elements
 //    - If you struggle with DOM drilling, use "ids" instead but watch the solution!
-
+const firstParagraphElement = document.body.children[2].children[1];
+console.log(firstParagraphElement);
+const thirdParagraphElement = document.body.children[2].children[3];
+// const thirdParagraphElement = firstParagraphElement.nextElementSibling.nextElementSibling;
+console.log(thirdParagraphElement);
 
 // 4) Change the functions from (2) such that:
 //    - The first button removes the third paragraph (i.e. the <p> prior to it)
 //    - The second button changes the background color of the first paragraph to blue
-
+function removingParagraph() {
+        console.dir(firstButtonElement);
+    }
+    
+    function addingBackgroundColor(event) {
+        console.dir(event.target);
+    }
+    
+    
+    firstButtonElement.addEventListener('click',removingParagraph );
+    secondButtonElement.addEventListener('click', addingBackgroundColor);
 
 // 5) Solve (4) both by changing the "inline styles" as well as by adding CSS classes
 //    Note: You'll have to add those classes to the styles.css file first!e
